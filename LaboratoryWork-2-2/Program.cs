fd61c68ebf34f0c4c6202b9ec91dda0751906ad9
Даниил Лямин <Niger120404@yandex.ru>
2022-10-07 11:39:35 +0300

    Добавьте файлы проекта.

---
+++ LaboratoryWork-2-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace LaboratoryWork_2_2
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			Stopwatch sw = new();
+			sw.Restart();
+			IsSimple(1000, 5000);
+			sw.Stop();
+			long x = sw.ElapsedMilliseconds;
+			Console.WriteLine(x);
+		}
+
+		private static void IsSimple(int firstnumber, int secondnumber)
+		{
+			List<int> list = new();
+			for (int i = firstnumber; i < secondnumber; i++)
+			{
+				list.Add(i);
+				for (int j = 2; j < i / 2; j++)
+				{
+					if (i % j == 0)
+					{
+						list.Remove(i);
+						break;
+					}
+				}
+
+			}
+			list.ForEach(x => Console.WriteLine(x));
+		}
+	}
+}
